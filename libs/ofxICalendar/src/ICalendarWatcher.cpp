acd12142bab0fbcd6845a1923f403e5a77712c1a
Christopher Baker <me@christopherbaker.net>
2014-04-25 15:17:49 +0200

    minor bugfix.

--- libs/ofxICalendar/src/ICalendarWatcher.cpp
+++ libs/ofxICalendar/src/ICalendarWatcher.cpp
@@ -37,7 +37,8 @@ ICalendarWatcher::ICalendarWatcher(ICalendar::SharedPtr calendar):
     _calendar(calendar),
     _updateInterval(DEFAULT_UPDATE_INTERVAL)
 {
-    _lastUpdate = Poco::Timestamp::Timestamp() - DEFAULT_UPDATE_INTERVAL.totalMicroseconds();
+    _lastUpdate = Poco::Timestamp::Timestamp();
+    _lastUpdate -= DEFAULT_UPDATE_INTERVAL.totalMicroseconds();
 
     ofAddListener(ofEvents().update, this, &ICalendarWatcher::update);
 }
