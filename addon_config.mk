d51e28c72e1e293f576df618f9a6b568c923667d
Christopher Baker <me@christopherbaker.net>
2019-04-07 23:19:03 -0600

    Add addons_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxICalendar
+	ADDON_DESCRIPTION = A calendar-based event scheduler. Useful for sending calendar events to your openFrameworks app.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "date" "time" "ical" ".ics" "calendar"
+	ADDON_URL = http://github.com/bakercp/ofxICalendar
+common:
+	ADDON_DEPENDENCIES = ofxTime ofxPoco
